b00acaeda66fca1de1ef3e505ed8d852cd98e04f
Igor <pa220282kiv@gmail.com>
2025-01-07 15:25:40 +0200

    Gallery v2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,29 +4,30 @@ import ImageGallery from './ImageGallery/ImageGallery.jsx';
 import axios from 'axios';
 import { useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
-import { RotatingLines } from 'react-loader-spinner';
+import ErrorMessage from './ErrorMessage/ErrorMessage.jsx';
+import LoadMoreBtn from './LoadMoreBtn/LoadMoreBtn.jsx';
+import Loader from './Loader/Loader.jsx';
 
-async function fetchArticles(query, setImageList, setLoading) {
+async function fetchArticles(query, page, setImageList, setLoading, setError) {
+  console.log(page);
+
   try {
     setLoading(true);
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: {
         client_id: 'fAybjthjUpXgAiQRgc05ZoRjv6Q4t5ldGkTZ1S9E_oo',
         query,
+        page,
         per_page: 3,
         orientation: 'landscape',
       },
     });
-    console.log(response);
+    // console.log(response);
+    setError(false);
     setImageList(response.data.results);
   } catch (error) {
+    setError(true);
     console.log(error);
-
-    toast.error('Something goes wrong. Please try again!', {
-      style: {
-        padding: '5px 20px',
-      },
-    });
   } finally {
     setLoading(false);
   }
@@ -35,40 +36,37 @@ async function fetchArticles(query, setImageList, setLoading) {
 function App() {
   const [imageList, setImageList] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState('1');
 
   const handleSubmit = (evt, query) => {
     evt.preventDefault();
     const form = evt.target;
     query = evt.target.search.value.trim();
+
     if (query === '') {
       toast.error('Please enter search term!', {
-        style: {
-          padding: '5px 20px',
-        },
+        style: { padding: '5px 20px' },
       });
       return;
     } else {
-      fetchArticles(query, setImageList, setLoading);
+      fetchArticles(query, page, setImageList, setLoading, setError);
     }
     form.reset();
+  };
+
+  const handleLoadMore = () => {
+    setPage(prev => prev + 1);
   };
 
   return (
     <>
       <SearchBar handleSubmit={handleSubmit} />
       {imageList.length > 0 && <ImageGallery imageList={imageList} />}
-      {loading && (
-        <RotatingLines
-          visible={true}
-          height="96"
-          width="96"
-          color="grey"
-          strokeWidth="5"
-          animationDuration="0.75"
-          ariaLabel="rotating-lines-loading"
-          wrapperStyle={{ justifyContent: 'center' }}
-        />
-      )}
+      {imageList.length > 0 && <LoadMoreBtn handleLoadMore={handleLoadMore} />}
+
+      {loading && <Loader />}
+      {error && <ErrorMessage />}
       <Toaster position="top-right" />
     </>
   );
